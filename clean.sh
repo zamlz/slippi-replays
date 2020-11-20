90663ba544aa3e7fc883ab4187f037caa2ca5c0a
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2020-11-19 18:22:46 -0800

    Updated script to only look at main files

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@
 
 # clean slippi files
 
-find . -type f -name "*.slp" | while read -r line; do
+find . -maxdepth 1 -type f -name "*.slp" | while read -r line; do
 
 year=$( echo $line | sed -r 's/\.\/Game_([0-9]{4})([0-9]{2})([0-9]{2}).*/\1/')
 month=$(echo $line | sed -r 's/\.\/Game_([0-9]{4})([0-9]{2})([0-9]{2}).*/\2/')
